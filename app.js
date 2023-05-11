ceec3eaace802e6b1e7829a856fd61e45a559903
Lvova Alina <lvova.aline@gmail.com>
2023-05-11 23:38:53 +0400

    feature: connect to mongodb

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
@@ -6,6 +7,13 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.listen(PORT, () => {
-    // Если всё работает, консоль покажет, какой порт приложение слушает
-    console.log(`App listening on port ${PORT}`)
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`)
 })
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
