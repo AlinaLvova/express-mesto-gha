78065c5b999d81be4052f313d7ce909d22235d0c
Lvova Alina <lvova.aline@gmail.com>
2023-05-12 00:59:22 +0400

    feature: create controllers and routes for users

--- app.js
+++ app.js
@@ -12,10 +12,15 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
 })
 
+//middleware для обработки данных в формате JSON
+app.use(express.json());
+
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true
   // useCreateIndex: true,
   // useFindAndModify: false
 });
+
+app.use('/', require('./routes/users'));
