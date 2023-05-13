213cbf3cfc1f2fc9f742075eb020654b1226d39c
Lvova Alina <lvova.aline@gmail.com>
2023-05-13 22:38:37 +0400

    fix: handle non-existent paths

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const handleErrors = require("./utils/errors");
+const {
+  NOT_FOUND_ERROR
+} = require("./utils/constants");
 
 // подключаемся к серверу mongo
 mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {
@@ -13,19 +16,17 @@ mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {
 });
 
 // Слушаем 3000 порт
-const { PORT = 3000, BASE_PATH } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
+
+// теперь клиент имеет доступ только к публичным файлам
+app.use(express.static(path.join(__dirname, 'public')));
 
 //middleware для обработки данных в формате JSON
 app.use(express.json());
 // для приёма веб-страниц внутри POST-запроса
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${PORT}`);
-});
 
 //middleware временное решение авторизации
 app.use((req, res, next) => {
@@ -37,3 +38,13 @@ app.use((req, res, next) => {
 
 app.use("/users", require("./routes/users"));
 app.use("/cards", require("./routes/cards"));
+
+// Middleware для обработки несуществующих путей
+app.use((req, res, next) => {
+  res.status(404).sendFile('404.html', { root: './public' });
+});
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`);
+});
