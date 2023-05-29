79a9baa0ed9dbf1aa55c2fa5e46775da86ec970b
Lvova Alina <lvova.aline@gmail.com>
2023-05-30 00:23:20 +0400

    feature: create login and signup routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const {
+  createUser, login,
+} = require('../controllers/users');
+
 const router = express.Router();
 
 const usersRouter = require('./users');
@@ -7,5 +11,8 @@ const cardsRouter = require('./cards');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
+
+router.post('/signin', login);
+router.post('/signup', createUser);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,12 @@
 const router = require('express').Router();
 
 const {
-  createUser, getUsers, getUserById, updateAvatar, updateProfile, login,
+  getUsers, getUserById, updateAvatar, updateProfile,
 } = require('../controllers/users');
 
-router.post('/', createUser);
 router.get('/', getUsers);
 router.get('/:userId', getUserById);
 router.patch('/me/avatar', updateAvatar);
 router.patch('/me', updateProfile);
-router.post('/login', login);
 
 module.exports = router;
