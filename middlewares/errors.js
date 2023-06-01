12d38f0eb75919a863ebf34cbb9207e0272e5c04
Lvova Alina <lvova.aline@gmail.com>
2023-06-01 16:32:10 +0400

    fix: change return

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -2,10 +2,8 @@ const { isCelebrateError } = require("celebrate");
 const { INTERNAL_SERVER_ERROR, BAD_REQUEST_ERROR } = require("../utils/constants");
 module.exports = (error, req, res, next) => {
   if (isCelebrateError(error)) {
-    return res.send({
-      statusCode: BAD_REQUEST_ERROR,
-      message: error.message,
-    });
+    return res.status(BAD_REQUEST_ERROR).send('Ошибка валидации.');
+  };
   }
   if (error.statusCode === 500) {
     res.status(INTERNAL_SERVER_ERROR).send({ message: "Ошибка по умолчанию." });
