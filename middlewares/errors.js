bb6e97b6bd20b2bd7c72c569338fd9a0802c6cea
Lvova Alina <lvova.aline@gmail.com>
2023-06-01 16:34:38 +0400

    fix: delete brace

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,10 +1,9 @@
 const { isCelebrateError } = require("celebrate");
-const { INTERNAL_SERVER_ERROR, BAD_REQUEST_ERROR } = require("../utils/constants");
+const { INTERNAL_SERVER_ERROR, BAD_REQUEST_ERROR } = fixrequire("../utils/constants");
 module.exports = (error, req, res, next) => {
   if (isCelebrateError(error)) {
     return res.status(BAD_REQUEST_ERROR).send('Ошибка валидации.');
   };
-  }
   if (error.statusCode === 500) {
     res.status(INTERNAL_SERVER_ERROR).send({ message: "Ошибка по умолчанию." });
   } else {
